e62236b8d843c8b9889d4a1155f9c29e84303671
Sheikh Nasir <sheikhnasir@yahoo.com>
2021-01-05 11:26:29 +0800

    first commit

---
+++ eLearning/MyCode/mylogin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace eLearning.MyCode
+{
+    [Activity(Label = "@string/title_mylogin", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class mylogin : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+        
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_login);
+
+            // Create your application here
+        }
+    }
+}
